d7ac382594c10160e955f267a175b6b313ee53a2
dedic <rockyida310@gmail.com>
2022-01-15 16:46:28 +0530

    notes ui improved

--- js/app.js
+++ js/app.js
@@ -8,16 +8,24 @@ let addBtn = document.getElementById('addBtn');
 
 addBtn.addEventListener("click",function(e){
 
-    let addTxt = document.getElementById("addTxt");
+    const newCardObj = {
+        Txt : document.getElementById("addTxt").value,
+        Title : document.getElementById("addTitle").value,
+        Link : document.getElementById("addLink").value
+    };
+
+
     let notes = localStorage.getItem("notes");
     if(notes == null){
         notesObj = [];
     }else{
         notesObj = JSON.parse(notes);
     }
-    notesObj.push(addTxt.value);
+    notesObj.push(newCardObj);
     localStorage.setItem("notes",JSON.stringify(notesObj));
     addTxt.value = "";
+    addTitle.value = "";
+    addLink.value = "";
     // console.log(notesObj);
     showNotes();
 })
@@ -32,15 +40,23 @@ function  showNotes(){
     }
     let html = "";
     notesObj.forEach(function(element , index) {
-        html += ` <div class="noteCard card mx-2 my-2" style="width: 18rem;">
-        <div class="card-body overflow-auto d-flex align-items-center flex-column" style="height: 300px; scrollbar-width: none;">
-          <h5 class="card-title">Note ${index+1}</h5>
-          <p class="card-text">
-          ${element}
-          </p>
-          <button id="${index}" onClick="deleteNote(this.id)" class="btn btn-primary position-absolute bottom-0 mb-3 ">Delete Note</button>
+        html += ` <div class="noteCard card mx-2 my-2 " style="width: 18rem;">
+        <div class="card-body overflow-auto d-flex align-items-center flex-column" style="height: 300px; ">
+          <h5 class="card-title">${element.Title}</h5>
+
+          <div class="text-wrap my-3  d-flex align-items-center flex-column" style="width: 14rem;">
+            <p class="card-text">
+                ${element.Txt}
+            </p>
+          </div>
+
+          <div class="badge badge-primary text-wrap my-3" style="width: 14rem;">
+            <a href=${element.Link} class="position-relative text-wrap">${element.Link}</a>
+          </div>
+          
+          <button id="${index}" onClick="deleteNote(this.id)" class="btn btn-primary position-absolute bottom-0 mb-4 ">Delete Note</button>
         </div>
-      </div>`;;
+      </div>`;
 
     });
 
